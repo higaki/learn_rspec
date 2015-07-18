fe78a3173c898fb01541c663f4cd35b64d02978f
Masaru HIGAKI <mas.higa@gmail.com>
2015-07-18 10:57:38 +0900

    example for example (pending)

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe "A calc" do
   # describe は入れ子にできる
   # describe と context は同じだが、「状況」を表わす場合は context を使う
   context "when normal mode" do
+    # it / example / specify は全て同じ。英語的な記述にこだわるなら使い分ける
     it 'given 2 and 3, returns 5' do
-      @calc.add(2, 3).to eq(5)
+      @calc.add(2, 3).should eq(5)
     end
 
     it 'given 5 and 8, returns 13' do
-      @calc.add(5, 8).to eq(13)
+      @calc.add(5, 8).should eq(13)
     end
   end
 
   context "when graph mode" do
-    it "draw graph" do
-    end
+    it "draw graph" # pending
   end
 end
