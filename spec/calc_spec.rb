74ab2a09bb1d51ffc889e09e66b23449572bb06e
Masaru HIGAKI <mas.higa@gmail.com>
2015-07-18 10:36:51 +0900

    refactor the spec at 2nd cycle

--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -1,13 +1,15 @@
 require 'calc'
 
 RSpec.describe "A calc" do
+  before do
+    @calc = Calc.new
+  end
+
   it 'given 2 and 3, returns 5' do
-    calc = Calc.new
-    calc.add(2, 3).to eq(5)
+    @calc.add(2, 3).to eq(5)
   end
 
   it 'given 5 and 8, returns 13' do
-    calc = Calc.new
-    calc.add(5, 8).to eq(13)
+    @calc.add(5, 8).to eq(13)
   end
 end
